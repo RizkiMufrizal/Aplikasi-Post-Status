5c509a86e9e09b45ede990dff5d412a241089b36
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-05-22 14:39:17 +0700

    Initial Post Status Client

---
+++ Post-Status-Client/www/js/services/AppService.js
@@ -0,0 +1,20 @@
+angular.module('App.services', [])
+  .factory('appService', function($http) {
+
+    var baseUrl = 'http://10.0.2.2:3000/api/post';
+
+    return {
+      getAllPost: function() {
+        return $http.get(baseUrl + '/AllPost');
+      },
+      savePost: function(post) {
+        return $http.post(baseUrl + '/SavePost', post);
+      },
+      commentPost: function(comment) {
+        return $http.post(baseUrl + '/Comment', comment);
+      },
+      likePost: function(like) {
+        return $http.post(baseUrl + '/Like', like);
+      }
+    }
+  });
