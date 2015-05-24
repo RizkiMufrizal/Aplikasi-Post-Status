8233348005b5f17d46c699fdf6b41959df0af2f6
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-05-24 20:56:52 +0700

    Update url

--- Post-Status-Client/www/js/services/UserService.js
+++ Post-Status-Client/www/js/services/UserService.js
@@ -1,16 +1,17 @@
 angular.module('App.services')
   .factory('userService', ['$http', function($http) {
 
-    var baseUrl = 'http://10.0.2.2:3000/api/user';
-    //var baseUrl = 'http://localhost:3000/api/user';
+    var baseUrl = 'https://aplikasipoststatus.herokuapp.com:443';
+    //var baseUrl = 'http://10.0.2.2:3000';
+    //var baseUrl = 'http://localhost:3000';
     var user = {};
 
     return {
       login: function(user) {
-        return $http.post(baseUrl + '/Login', user);
+        return $http.post(baseUrl + '/api/user/Login', user);
       },
       signUp: function(user) {
-        return $http.post(baseUrl + '/SignUp', user);
+        return $http.post(baseUrl + '/api/user/SignUp', user);
       },
       setUser: function(email, nama) {
         user.email = email;
